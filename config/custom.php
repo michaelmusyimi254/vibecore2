bec11c70258042b18bca540a9a0c2e3c8d757cc3
michaelmusyimi254 <michaelmusyimi@gmail.com>
2025-07-12 01:11:47 +0300

    colours

--- config/custom.php
+++ config/custom.php
@@ -8,7 +8,7 @@
 return [
   'custom' => [
     'myLayout' => 'vertical', // Layout type: 'vertical' (default), 'horizontal'
-    // 'primaryColor' => '#FF4646', // Set the primary color
+    'primaryColor' => '#ff0000', // Set the primary color (corporate red)
     'myTheme' => 'light', // Theme options: 'light' (default), 'dark', 'system'
     'mySkins' => 'default', // Skin options: 'default', 'bordered'
     'hasSemiDark' => false, // Semi-dark mode: true/false (false by default)
