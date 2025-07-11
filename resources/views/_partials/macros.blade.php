902573789cd83c57446ebdf17ae452b1e89c4d29
michaelmusyimi254 <michaelmusyimi@gmail.com>
2025-07-11 18:17:00 +0300

    .png

--- resources/views/_partials/macros.blade.php
+++ resources/views/_partials/macros.blade.php
@@ -2,5 +2,5 @@
     <!-- Light logo -->
     <img src="{{ asset('images/logo-light.png') }}" alt="Logo" class="logo-light" height="40">
     <!-- Dark logo -->
-    <img src="{{ asset('images/logo-dark.svg') }}" alt="Logo" class="logo-dark" height="40">
+    <img src="{{ asset('images/logo-dark.png') }}" alt="Logo" class="logo-dark" height="40">
 </a>
